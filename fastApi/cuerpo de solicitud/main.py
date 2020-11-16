26819afae67a47ab6d88a8dc0c9faad343be2d29
Jorgegarciasamper <jordi.gs.1986@gmail.com>
2020-11-16 03:22:13 -0800

    first commit

---
+++ fastApi/cuerpo de solicitud/main.py
@@ -0,0 +1,26 @@
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel #importación de BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None #Parámetro no obligatorio
+    price: float
+    tax: Optional[float] = None
+
+app = FastAPI()
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+    
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: Optional[str] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
